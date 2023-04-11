cae95f3cf0adc88241df97c9d02e0dc980d6bc05
Prashant Shinde <shindeprashant1812@gmail.com>
2023-04-11 23:08:12 +0530

    my first commit

---
+++ pattan.java
@@ -0,0 +1,16 @@
+class Demo{
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                if(a++%2==0){
+                    System.out.print("* ");
+                }else{
+                System.out.print(b++ +" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
